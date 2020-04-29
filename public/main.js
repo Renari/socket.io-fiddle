05d380f1f14afa0aa7c4982c887027b1636f2812
Arie Miller <amiller@soliddynamics.net>
2020-04-28 22:02:56 -0400

    connection drops on large files

--- public/main.js
+++ public/main.js
@@ -10,4 +10,14 @@
     console.log('connect ' + socket.id);
   }
 
+  const uploadElement = document.getElementById('upload');
+  uploadElement.addEventListener('change', (event) => {
+    const reader = new FileReader();
+    reader.addEventListener('loadend', function() {
+      console.log('finished reading file');
+      socket.emit('file', reader.result);
+    });
+    reader.readAsArrayBuffer(uploadElement.files[0]);
+  });
+
 })();
